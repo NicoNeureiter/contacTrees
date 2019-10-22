ffae69cb0e0ed6087440467479eb9c4ce10bfcdb
NicoNeureiter <nico.neureiter@gmail.com>
2019-10-22 21:23:00 +1300

    Included newick parsers from Bacter.

---
+++ src/contactrees/util/parsers/ExtendedNewickBaseVisitor.java
@@ -0,0 +1,91 @@
+// Generated from /Users/vaughant/code/beast_and_friends/bacter/src/bacter/util/parsers/ExtendedNewick.g4 by ANTLR 4.7
+package contactrees.util.parsers;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link ExtendedNewickVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class ExtendedNewickBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ExtendedNewickVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTree(ExtendedNewickParser.TreeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNode(ExtendedNewickParser.NodeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPost(ExtendedNewickParser.PostContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitLabel(ExtendedNewickParser.LabelContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitHybrid(ExtendedNewickParser.HybridContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMeta(ExtendedNewickParser.MetaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAttrib(ExtendedNewickParser.AttribContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAttribValue(ExtendedNewickParser.AttribValueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNumber(ExtendedNewickParser.NumberContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVector(ExtendedNewickParser.VectorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitString(ExtendedNewickParser.StringContext ctx) { return visitChildren(ctx); }
+}
---
+++ src/contactrees/util/parsers/ExtendedNewickVisitor.java
@@ -0,0 +1,79 @@
+// Generated from /Users/vaughant/code/beast_and_friends/bacter/src/bacter/util/parsers/ExtendedNewick.g4 by ANTLR 4.7
+package contactrees.util.parsers;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ExtendedNewickParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ExtendedNewickVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#tree}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTree(ExtendedNewickParser.TreeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#node}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNode(ExtendedNewickParser.NodeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#post}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPost(ExtendedNewickParser.PostContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#label}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitLabel(ExtendedNewickParser.LabelContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#hybrid}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitHybrid(ExtendedNewickParser.HybridContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#meta}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMeta(ExtendedNewickParser.MetaContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#attrib}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAttrib(ExtendedNewickParser.AttribContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#attribValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAttribValue(ExtendedNewickParser.AttribValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#number}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumber(ExtendedNewickParser.NumberContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#vector}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVector(ExtendedNewickParser.VectorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExtendedNewickParser#string}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitString(ExtendedNewickParser.StringContext ctx);
+}
