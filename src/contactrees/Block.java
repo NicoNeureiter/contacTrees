613f7c24484051656c84296fe4bace3bf4a0da02
NicoNeureiter <nico.neureiter@gmail.com>
2019-10-22 19:56:38 +1300

    Made Block class more lean and independent. E.g. no reference to network.

--- src/contactrees/Block.java
+++ src/contactrees/Block.java
@@ -6,11 +6,13 @@ package contactrees;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.IllegalArgumentException;
+
+import org.w3c.dom.Node;
 
 import beast.core.Description;
-import beast.core.Input;
 import beast.core.StateNode;
+import contactrees.util.Util;
+
 
 /**
  * @author Nico Neureiter <nico.neureiter@gmail.com>
@@ -18,102 +20,156 @@ import beast.core.StateNode;
 @Description("A block which is affected by a marginal tree and defined through a set of conversions wihtin a network.")
 public class Block extends StateNode { 
 
-	public Input<ConversionGraph> networkInput = new Input<>(
-			"network",
-			"The network on which the conversion moves are defined.",
-			Input.Validate.REQUIRED);
-	
-	ConversionGraph acg;
-	List<Conversion> convs;
+	// The indices (in the ConversionList) of the conversions affecting this block. 
+	List<Integer> convIds, convIdsStored;
 
 	@Override
 	public void initAndValidate() {
-		acg = networkInput.get();
-		convs = new ArrayList<Conversion>();
+		convIds = new ArrayList<Integer>();
+        convIdsStored = new ArrayList<>();
 	}
 
-    /**
-     * StateNode implementation *
-     */
-	@Override
-	protected void store() {
-		// TODO Auto-generated method stub
-
+	/**
+	 * Add a conversion over which this block moves in its history.
+	 * @param The new conversion.
+	 */
+	public void addMove(Conversion conv) {
+		convIds.add(conv.getID());
+	}
+	
+	
+	/**
+	 * Remove a conversion move from this blocks history.
+	 * @param The conversion to be removed.
+	 */
+	public void removeMove(Conversion conv) {
+		convIds.remove(conv.getID());
 	}
 
-	@Override
-	public void restore() {
-		// TODO Auto-generated method stub
-
+	/**
+	 * Evaluate whether this block was affected by the given conversion.
+	 * @param The conversion in question.
+	 * @return "true" iff the block moved over the conversion.
+	 */
+	public boolean isAffected(Conversion conv) {
+		// TODO resolve overshadowed conversions somewhere
+		return convIds.contains(conv.getID()); 
+	}
+	
+	/**
+	 * Number of conversions over which this block moves.
+	 * @return Number of conversions.
+	 */
+	public int size() {
+		return convIds.size();
 	}
 
+	
+	/*
+	 * Loggable implementations 
+	 */
+	
+	/**
+	 * init() and close() can stay empty.
+	 */
 	@Override
-	public void setEverythingDirty(boolean isDirty) {
-		// TODO Auto-generated method stub
-
+	public void init(PrintStream out) {}
+	@Override
+	public void close(PrintStream out) {}
+	
+	@Override
+	public int getDimension() {
+		return 1;
 	}
 
+	/*
+	 * StateNode implementations
+	 */
 	@Override
-	public StateNode copy() {
-		// TODO Auto-generated method stub
-		return null;
+	public Block copy() {
+		Block copy = new Block();
+		
+		copy.setID(getID());
+		copy.convIds = Util.deepCopyIntegers(convIds);
+		copy.convIdsStored = Util.deepCopyIntegers(convIdsStored);
+		
+		return copy;
 	}
 
 	@Override
 	public void assignTo(StateNode other) {
-		// TODO Auto-generated method stub
-
+		final Block block = (Block) other;
+		block.setID(getID());
+		block.convIds = Util.deepCopyIntegers(convIds);
+		block.convIdsStored = Util.deepCopyIntegers(convIdsStored);
 	}
 
 	@Override
 	public void assignFrom(StateNode other) {
-		// TODO Auto-generated method stub
+		final Block block = (Block) other;
+		setID(block.getID());
+		convIds = Util.deepCopyIntegers(block.convIds);
+		convIdsStored = Util.deepCopyIntegers(block.convIdsStored);
+	}
 
+	@Override
+	protected void store() {
+		convIdsStored = Util.deepCopyIntegers(convIds);
+	}
+
+	@Override
+	public void restore() {
+		List<Integer> tmp = convIds;
+		convIds = convIdsStored;
+		convIdsStored = tmp;
+		
+//		ConversionList convList = acg.getConversions();
+//		List<Integer> toStore = convIdsStored;
+//		
+//		// Remove all active conversion that are not stored
+//		for (int id : convIds) {
+//			if (!convIdsStored.contains(id)) {
+//				toStore.add(id);
+//				removeConversion(convList.get(id));
+//			}
+//		}
+//		
+//		// Add all stored conversions that are not active
+//		for (int id : convIdsStored) {
+//			if (!convIds.contains(id)) {
+//				addConversion(convList.get(id));
+//			}
+//		}
+//		
+//		// Add the removed conversions to the stored ones
+//		for (int id : toStore) {
+//			convIdsStored.add(id);
+//		}
 	}
 
 	@Override
 	public void assignFromFragile(StateNode other) {
-		// TODO Auto-generated method stub
-
+		assignFrom(other);
+	}
+	
+	@Override
+	public double getArrayValue(int dim) {
+		return convIds.size();
 	}
 
 	@Override
-	public void fromXML(final org.w3c.dom.Node node) {
-		// TODO Auto-generated method stub
+	public void setEverythingDirty(boolean isDirty) {
+		setSomethingIsDirty(isDirty);
+	}
 
+	@Override
+	public void fromXML(Node node) {
+		// TODO Auto-generated method stub	
 	}
 
 	@Override
 	public int scale(double scale) {
-		throw new IllegalArgumentException("Marginal tree moves can not be scaled");
-	}
+		throw new UnsupportedOperationException("StateNode method scale() not applicable to Block."); 
+	}	
 	
-	/**
-	 * Loggable implementation *
-	 */
-	@Override
-	public void init(PrintStream out) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void close(PrintStream out) {
-		// TODO Auto-generated method stub
-	}
-	
-	/**
-	 * Function implementation *
-	 */
-	
-	@Override
-	public int getDimension() {
-		// TODO ?  
-		return convs.size();
-	}
-
-	@Override
-	public double getArrayValue(int dim) {
-		// TODO ?
-		return convs.get(dim).height;
-	}
 }
