e4d77614a8d9c6487e2c77fa3db41ee5d4ccb554
NicoNeureiter <nico.neureiter@gmail.com>
2019-10-30 13:33:39 +1300

    Added ACG simulator

---
+++ src/contactrees/model/ACGSimulator.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2013 Tim Vaughan <tgvaughan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package contactrees.model;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+@Description("Simulates an ACG - can be used for chain initialization or for "
+        + "sampler validation.")
+public class ACGSimulator extends beast.core.Runnable {
+
+    public Input<SimulatedACGWithBlocks> simACGInput = new Input<>(
+            "simACG",
+            "Simulated recombination graph.",
+            Input.Validate.REQUIRED);
+
+    public Input<Integer> nSimsInput = new Input<>(
+            "nSims",
+            "Number of ARGs to simulate.",
+            Input.Validate.REQUIRED);
+    
+    public Input<List<Logger>> loggersInput = new Input<>(
+            "logger",
+            "Logger used to write results to screen or disk.",
+            new ArrayList<>());
+    
+    @Override
+    public void initAndValidate() { }
+
+    @Override
+    public void run() throws Exception {
+
+        // Initialise loggers
+        for (Logger logger : loggersInput.get()) {
+            logger.init();
+        }
+
+        for (int i=0; i<nSimsInput.get(); i++) {
+            
+            if (i>0)
+                simACGInput.get().initAndValidate();
+            
+            // Log state
+            for (Logger logger : loggersInput.get()) {
+                logger.log(i);
+            }
+        }
+        
+        // Finalize loggers
+        for (Logger logger : loggersInput.get()) {
+            logger.close();
+        }
+    }
+    
+}
---
+++ src/contactrees/model/SimulatedACGWithBlocks.java
@@ -0,0 +1,336 @@
+/*
+ * Copyright (C) 2013 Tim Vaughan <tgvaughan@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package contactrees.model;
+
+import contactrees.ACGWithMetaDataLogger;
+import contactrees.Block;
+import contactrees.BlockSet;
+import contactrees.CFEventList;
+import contactrees.Conversion;
+import contactrees.ConversionGraph;
+import beast.core.BEASTObject;
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.RealParameter;
+import beast.evolution.tree.Node;
+import beast.evolution.tree.Tree;
+import beast.evolution.tree.coalescent.PopulationFunction;
+import beast.math.Binomial;
+import beast.util.Randomizer;
+import feast.nexus.NexusBlock;
+import feast.nexus.NexusBuilder;
+import feast.nexus.TaxaBlock;
+import feast.nexus.TreesBlock;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.math3.distribution.BinomialDistribution;
+
+/**
+ * @author Tim Vaughan <tgvaughan@gmail.com>
+ */
+@Description("Simulates an ARG under the full ClonalOrigin model - can be used"
+    + " for chain initialization or for sampler validation.")
+public class SimulatedACGWithBlocks extends BEASTObject {
+
+	public Input<ConversionGraph> networkInput = new Input<>(
+			"network",
+			"The conversion graph to be logged.",
+			Input.Validate.REQUIRED);
+    
+    public Input<BlockSet> blockSetInput = new Input<>(
+    		"blockSet",
+    		"The set of blocks evolving along the ACG.",
+    		Input.Validate.REQUIRED);
+	
+    public Input<Double> conversionRateInput = new Input<>(
+            "conversionRate",
+            "Conversion rate parameter.",
+            Input.Validate.REQUIRED);
+    
+    public Input<Double> moveProbInput = new Input<>(
+            "moveProb",
+            "Probability of a block moving over a conversion edge.",
+            Input.Validate.REQUIRED);
+
+    public Input<PopulationFunction> popFuncInput = new Input<>(
+            "populationModel",
+            "Demographic model to use.");
+    
+    public Input<Double> birthRateInput = new Input<>(
+            "birthRate",
+            "Birth rate of the Yule tree prior.",
+            Input.Validate.XOR, popFuncInput);
+
+    public Input<Tree> clonalFrameInput = new Input<>(
+            "clonalFrame",
+            "Optional tree specifying fixed clonal frame.");
+
+    public Input<String> outputFileNameInput = new Input<>(
+            "outputFileName",
+            "If provided, simulated ARG is additionally written to this file.");
+
+    private ConversionGraph acg;
+    private BlockSet blockSet;
+    private Double conversionRate, birthRate, moveProb;
+    private PopulationFunction popFunc;
+    
+//    public SimulatedACGWithBlocks() {
+//        acg.m_taxonset.setRule(Input.Validate.REQUIRED);
+//    }
+    
+    @Override
+    public void initAndValidate() {
+    	blockSet = blockSetInput.get();
+        acg = networkInput.get();
+        conversionRate = conversionRateInput.get();
+        birthRate = birthRateInput.get();
+        moveProb = moveProbInput.get();
+        popFunc = popFuncInput.get();
+        
+        acg.initAndValidate();
+        blockSet.initAndValidate();
+
+        if (clonalFrameInput.get() == null)
+            simulateClonalFrame();
+        else
+            acg.assignFromWithoutID(clonalFrameInput.get());
+        
+        // Need to do this here as this sets the tree object that the nodes
+        // point to, so without it they point to the dummy tree created by
+        // super.initAndValidate().
+        acg.initTreeArrays();
+        
+        // Generate recombinations
+        generateConversions();
+        
+        // Write output file
+        if (outputFileNameInput.get() != null) {
+
+            NexusBuilder nexusBuilder = new NexusBuilder();
+            
+            nexusBuilder.append(new TaxaBlock(acg.m_taxonset.get()));
+            
+            nexusBuilder.append((new TreesBlock() {
+                @Override
+                public String getTreeString(Tree tree) {
+                	ACGWithMetaDataLogger acgLogger = ACGWithMetaDataLogger.getACGWMDLogger((ConversionGraph) tree, blockSet);
+                    return acgLogger.getExtendedNewick();
+                }
+            }).addTree(acg, "simulatedARG"));
+            
+            nexusBuilder.append(new NexusBlock() {
+
+                @Override
+                public String getBlockName() {
+                    return "contactrees";
+                }
+
+                @Override
+                public List<String> getBlockLines() {
+                    List<String> lines = new ArrayList<>();
+
+                    String blockSetLine = "blockSet";
+                    for (Block block: blockSet.getBlocks())
+                    	blockSetLine += " " + block.getID();
+                    lines.add(blockSetLine);
+
+                    lines.add("clonalframe_labeled " + acg.getRoot().toNewick());
+                    lines.add("clonalframe_numbered " + acg.getRoot().toShortNewick(true));
+                    for (Conversion conv : acg.getConversions()) {
+                        lines.add("conversion"
+                        		+ " node1=" + conv.getNode1().getNr()
+                                + " node2=" + conv.getNode2().getNr()
+                                + " affectedBlocks=" + blockSet.getAffectedBlocks(conv));
+                    }
+
+                    return lines;
+                }
+            });
+
+            try (PrintStream pstream = new PrintStream(outputFileNameInput.get())) {
+                nexusBuilder.write(pstream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Use coalescent model to simulate clonal frame.
+     */
+    private void simulateClonalFrame() {
+    	if (popFunc != null) {
+	
+	        // Initialize leaf nodes
+	        List<Node> leafNodes = new ArrayList<>();
+	        for (int i=0; i<acg.m_taxonset.get().getTaxonCount(); i++) {
+	            Node leaf = new Node();
+	            leaf.setNr(i);
+	            leaf.setID(acg.m_taxonset.get().getTaxonId(i));
+	                        
+	            if (acg.hasDateTrait())
+	                leaf.setHeight(acg.getDateTrait().getValue(leaf.getID()));
+	            else
+	                leaf.setHeight(0.0);
+	            
+	            leafNodes.add(leaf);
+	        }
+	        
+	        // Create and sort list of inactive nodes
+	        List<Node> inactiveNodes = new ArrayList<>(leafNodes);
+	        Collections.sort(inactiveNodes, (Node n1, Node n2) -> {
+	            if (n1.getHeight()<n2.getHeight())
+	                return -1;
+	            
+	            if (n1.getHeight()>n2.getHeight())
+	                return 1;
+	            
+	            return 0;
+	        });
+	        
+	        List<Node> activeNodes = new ArrayList<>();
+	        
+	        double tau = 0.0;
+	        int nextNr = leafNodes.size();
+	        while (true) {
+	            
+	            // Calculate coalescence propensity
+	            int k = activeNodes.size();
+	            double chi = 0.5*k*(k-1);
+	            
+	            // Draw scaled coalescent time
+	            if (chi>0.0)
+	                tau += Randomizer.nextExponential(chi);
+	            else
+	                tau = Double.POSITIVE_INFINITY;
+	            
+	            // Convert to real time
+	            double t = popFunc.getInverseIntensity(tau);
+	            
+	            // If new time takes us past next sample time, insert that sample
+	            if (!inactiveNodes.isEmpty() && t>inactiveNodes.get(0).getHeight()) {
+	                Node nextActive = inactiveNodes.remove(0);
+	                activeNodes.add(nextActive);
+	                tau = popFunc.getIntensity(nextActive.getHeight());
+	                continue;
+	            }
+	            
+	            // Coalesce random pair of active nodes.
+	            Node node1 = activeNodes.remove(Randomizer.nextInt(k));
+	            Node node2 = activeNodes.remove(Randomizer.nextInt(k-1));
+	            
+	            Node parent = new Node();
+	            parent.addChild(node1);
+	            parent.addChild(node2);
+	            parent.setHeight(t);
+	            parent.setNr(nextNr++);
+	            
+	            activeNodes.add(parent);
+	            
+	            if (inactiveNodes.isEmpty() && activeNodes.size()<2)
+	                break;
+	        }
+	        
+	        // Remaining active node is root
+	        acg.setRoot(activeNodes.get(0));
+	        
+    	} else {
+    		assert (birthRate != null);
+    		throw new RuntimeException("Yule model not yet implemented.");
+    	}
+    }
+    
+    private void generateConversions() {
+
+        // Draw number of conversions:
+    	double nConvMean = conversionRate * acg.getClonalFramePairedLength();
+        int nConv = (int) Randomizer.nextPoisson(nConvMean);
+
+        // Generate conversions:
+        for (int i=0; i<nConv; i++) {
+        	
+            Conversion conv = acg.addNewConversion();
+            associateConversionWithCF(conv);
+            
+            // Choose affected blocks:
+            int nAffected = sampleBinomial(blockSet.getBlockCount(), moveProb);
+            int[] shuffledBlockIdxs = Randomizer.shuffled(nAffected);
+            for (int j=0; j<nAffected; j++) {
+            	int blockIdx = shuffledBlockIdxs[j];
+            	blockSet.getBlocks().get(blockIdx).addMove(conv);
+            }
+        }
+    }
+    
+    private int sampleBinomial(int n, double p) {
+    	return (new BinomialDistribution(n, p)).sample();
+    }
+    
+    
+    /**
+     * Associates recombination with the clonal frame, selecting points of
+     * departure and arrival.
+     * 
+     * @param conv recombination to associate
+     */
+    private void associateConversionWithCF(Conversion conv) {
+    
+        // Select departure point (node1 and height)
+        double u = Randomizer.nextDouble() * acg.getClonalFrameLength();
+
+        // TODO Change the way we attach edges: Choose event interval by paired-length
+        
+        for (Node node : acg.getNodesAsArray()) {
+            if (node.isRoot())
+                continue;
+            
+            if (u<node.getLength()) {
+                conv.setHeight(node.getHeight() + u);
+                conv.setNode1(node);
+                break;  
+            } 
+            else {
+                u -= node.getLength();
+            }
+        }
+        
+        assert conv.getHeight() < acg.getRoot().getHeight();
+        
+        // Select arrival point (node2 given the height and node1)
+        Set<Node> activeLineages = acg.getLineagesAtHeight(conv.getHeight());
+        activeLineages.remove(conv.getNode1());
+        
+        // Sample the arrival point uar from the active lineages (without node 1)
+        int choice = Randomizer.nextInt(activeLineages.size());
+        int i=0;
+        for (Node node : activeLineages) {
+            if (i == choice) {
+            	conv.setNode2(node);
+            	break;
+            }
+            i++;
+        }
+    }
+    
+}
