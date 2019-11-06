987d952da88326549b22fa1bd67b025e66e46918
NicoNeureiter <nico.neureiter@gmail.com>
2019-11-06 17:55:28 +1300

    Added some utility functions

--- src/contactrees/util/Util.java
+++ src/contactrees/util/Util.java
@@ -5,7 +5,10 @@ package contactrees.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+import beast.evolution.tree.Node;
 import beast.util.Randomizer;
 
 /**
@@ -37,6 +40,79 @@ public class Util {
 		}
 		assert population.size() == 0;
 		throw new RuntimeException("Can not sample from empty set.");
+	}
+	
+	static public void sortByHeight(List<Node> nodes, boolean reverse) {
+		if (reverse) {
+	        Collections.sort(nodes, (Node n1, Node n2) -> {
+	            if (n1.getHeight()<n2.getHeight())
+	                return -1;
+	            if (n1.getHeight()>n2.getHeight())
+	                return 1;
+	            return 0;
+	        });
+		} else {
+	        Collections.sort(nodes, (Node n1, Node n2) -> {
+	            if (n1.getHeight()<n2.getHeight())
+	                return 1;
+	            if (n1.getHeight()>n2.getHeight())
+	                return -1;
+	            return 0;
+	        });
+		}
+		
+	}
+	
+	static public double logFactorial(int n) {
+		double res = 0;
+		for (int i=1; i<=n; i++) {
+			res += Math.log(i);
+		}
+		return res;
+	}
+	
+	static public double sum(double[] values) {
+		double total = 0;
+		for (double x : values) total += x; 
+		return total;
+	}
+	
+	static public double sum(Iterable<Double> values) {
+		double total = 0;
+		for (double x : values) total += x; 
+		return total;
+	}
+	
+	static public int sampleCategorical(double[] weights) {
+		double weightSum = sum(weights);
+		double u = weightSum * Randomizer.nextDouble();
+		
+		int i = 0;
+		for (double w : weights) {
+			if (u < w)
+				return i;
+			
+			u -= w;
+			i++;
+		}
+	
+		throw new RuntimeException("Can not sample from an empty collection.");
+	}
+	
+	static public int sampleCategorical(Iterable<Double> weights) {
+		double weightSum = sum(weights);
+		double u = weightSum * Randomizer.nextDouble();
+		
+		int i = 0;
+		for (double w : weights) {
+			if (u < w)
+				return i;
+			
+			u -= w;
+			i++;
+		}
+	
+		throw new RuntimeException("Can not sample from an empty collection.");
 	}
 	
 }
