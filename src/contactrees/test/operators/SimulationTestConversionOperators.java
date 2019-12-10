31f3b6bb15a89c3d12f0316e38048624163d9850
NicoNeureiter <nico.neureiter@gmail.com>
2019-12-10 13:03:29 +1300

    Added testcases for new operators

--- src/contactrees/test/operators/SimulationTestConversionOperators.java
+++ src/contactrees/test/operators/SimulationTestConversionOperators.java
@@ -3,20 +3,30 @@ package contactrees.test.operators;
 import org.junit.Test;
 
 public class SimulationTestConversionOperators extends TestSingleOperator {
-	
+  
     @Test
     public void testAddRemoveConversion() throws Exception {
         testStationarity("AddRemoveConversion");
     }
+
+    @Test
+    public void testAddRemoveConversionGibbs() throws Exception {
+        testStationarity("AddRemoveConversionGibbs");
+    }
     
     @Test
-    public void testMoveConversion() throws Exception {
-        
+    public void testConvertedEdgeSlide() throws Exception {
+        testStationarity("ConvertedEdgeSlide");
     }
     
     @Test
     public void testResampleConversionMoves() throws Exception {
         testStationarity("ResampleConversionMoves");
+    }
+    
+    @Test
+    public void testGibbsSampleMovesPerConversion() throws Exception {
+        testStationarity("GibbsSampleMovesPerConversion");
     }
     
 }
