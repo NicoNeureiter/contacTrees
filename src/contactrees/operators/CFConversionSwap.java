c682197a28f3bdd087f89c3889d2ee299432a533
NicoNeureiter <nico.neureiter@gmail.com>
2020-08-21 16:43:11 +0200

    Implemented two new operators.

---
+++ src/contactrees/operators/CFConversionSwap.java
@@ -0,0 +1,130 @@
+package contactrees.operators;
+
+import contactrees.Block;
+import contactrees.Conversion;
+import contactrees.util.Util;
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.BooleanParameter;
+import beast.evolution.tree.Node;
+import beast.util.Randomizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.attribute.standard.MediaSize.Other;
+
+/**
+ * Swap attachment points of a subtree and a conversion, keeping heights constant.
+ * This can preserve the topology of all marginal trees, 
+ * but changes marginal node heights (depends on the sampled block moves). 
+ *
+ * @author Nico Neureiter
+ */
+@Description("CF/conversion swap operator.")
+public class CFConversionSwap extends CFOperator {
+
+    Input<Boolean> resampleMovesInput = new Input<>(
+            "resampleMoves",
+            "Whether to sample new blocks to be moved over the conversion (set to ´true´) or simply invert the previos moves (set to ´false´)",
+            false);
+    
+    @Override
+    public void initAndValidate() {
+        super.initAndValidate();
+    }
+
+    @Override
+    public double proposal() {
+        double logHGF = 0.0;
+
+        // Determine whether we can apply this operator:
+        if (acg.getLeafNodeCount()<3 || acg.getConvCount()==0)
+            return Double.NEGATIVE_INFINITY;
+
+        // Acquire list of conversions compatible with swap:
+        Conversion conv = acg.getConversions().getRandomConversion();
+
+        // Cancel if conv is a cherry-edge
+        if (conv.getNode1().getParent() == conv.getNode2().getParent())
+            return Double.NEGATIVE_INFINITY;
+        
+        Node child = conv.getNode1();
+        Node parent = child.getParent();
+        Node sibling = Util.getOtherChild(parent, child);
+        Node newSibling = conv.getNode2();
+        
+        // Cancel if a speciation happens below parent and above conv.height 
+        if (sibling.getHeight() > conv.getHeight())
+            return Double.NEGATIVE_INFINITY;
+
+        // Cancel if speciation happens above conv and below parent.height
+        if (newSibling.getParent().getHeight() < parent.getHeight())
+            return Double.NEGATIVE_INFINITY;
+
+        // No nodes in the way  =>  We can simply:
+        //    * Re-attach parent above new sibling
+        //    * Re-attach conv above old sibling
+        
+        // Re-attach parent above new sibling
+        Node grandParent = parent.getParent();
+        Node newGrandParent = newSibling.getParent();
+        
+        grandParent.removeChild(parent);
+        grandParent.addChild(sibling);
+        newGrandParent.removeChild(newSibling);
+        newGrandParent.addChild(parent);
+        parent.removeChild(sibling);
+        parent.addChild(newSibling);
+        
+        assert Util.getSibling(child) == newSibling;
+        assert parent.getParent() == newGrandParent;
+        assert sibling.getParent() == grandParent;
+        assert sibling != child;
+        
+        // Re-attach conv above old sibling
+        conv.setNode2(sibling);
+        
+        // Move other conversions from parent to sibling
+        for (Conversion c : acg.getConversions()) {
+            if (c.getNode1() == parent)
+                c.setNode1(sibling);
+            if (c.getNode2() == parent)
+                c.setNode2(sibling);
+        }
+        
+        // Update conversion which were previously attached to "newSibling", but are now above "parent".
+        for (Conversion c : acg.getConversions()) {
+            if (c == conv)
+                continue;
+            if (c.getNode1() == newSibling && c.getHeight() > parent.getHeight())
+                c.setNode1(parent);
+            if (c.getNode2() == newSibling && c.getHeight() > parent.getHeight())
+                c.setNode2(parent);
+        }
+        
+        if (resampleMovesInput.get()) {
+            logHGF += drawAffectedBlocksGibbs(conv);
+        } else {
+            invertAffectedBlocks(conv);
+        }
+        
+        assert !acg.isInvalid() : "CFCS proposed invalid state.";
+
+        return logHGF;
+    }
+    
+    /**
+     * Invert all block-moves of the given conversion (i.e. remove the present ones and add all other block moves).
+     * @param conv
+     */
+    protected void invertAffectedBlocks(Conversion conv) {
+        for (Block block : blockSet) {
+            if (block.isAffected(conv)) {
+                block.removeMove(conv);
+            } else {
+                block.addMove(conv);
+            }
+        }
+    }
+}
---
+++ src/contactrees/operators/ConversionSplit.java
@@ -0,0 +1,196 @@
+package contactrees.operators;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.RealParameter;
+import beast.evolution.tree.Node;
+import beast.util.Randomizer;
+import contactrees.Block;
+import contactrees.Conversion;
+
+/**
+ * 
+ * 
+ * 
+ * @author Nico Neureiter
+ */
+@Description("Operator which splits or merges conversions")
+public class ConversionSplit extends ConversionCreationOperator {
+
+    final public Input<Boolean> flipInput = new Input<>(
+            "flip",
+            "Flip the the duplicated conversion edge to point in the oposite direction (compared to the original one).",
+            false
+            );
+
+//    final public Input<RealParameter> pDoubleMoveInput = new Input<>(
+//            "pDoubleMove",
+//            "The probability that a move is added to both edges whet spliting a conversion.",
+//            new RealParameter(0.2));
+    
+    @Override
+    public double proposal() {
+        
+        if (Randomizer.nextBoolean())
+            return splitProposal();
+        else
+            return mergeProposal();
+    }
+
+    protected double splitProposal() {
+        double logHGF = 0;
+        int nConv = acg.getConvCount();
+        
+        // The split move requires at least one existing conversion
+        if (nConv == 0)
+            return Double.NEGATIVE_INFINITY;
+        
+        // Chose the conversion to split
+        Conversion conv = acg.getConversions().getRandomConversion();
+        // Create the duplicate
+        Conversion convNew = acg.addDuplicateConversion(conv);
+        // Update Hastings ratio:
+        //        logHGF -= Math.log(1.0/nConv);
+        //        logHGF += Math.log(1.0/((nConv+1)*nConv));
+        logHGF -= Math.log(nConv+1);
+        
+        
+        
+        if (flipInput.get())
+            flipDirection(convNew);
+        
+        // Sample new height uniformly on the current lineages
+        double hMin = Math.max(conv.getNode1().getHeight(),
+                               conv.getNode2().getHeight());
+        double hMax = Math.min(conv.getNode1().getParent().getHeight(),
+                               conv.getNode2().getParent().getHeight());
+        double heightNew = Randomizer.uniform(hMin, hMax);
+        convNew.setHeight(heightNew);
+        
+        // Update Hastings ratio for sampled height
+        logHGF -= Math.log(1.0/(hMax - hMin));
+        
+        assert hMin <= conv.getHeight() && conv.getHeight() <= hMax : 
+            "Conversion height must be in validHeightRange";
+        
+          
+        // Choose which blocks-moves stay on conv and which are moved to convNew
+        double pDoubleMove = pMoveInput.get().getValue();
+        List<Block> blocksToRemove = new LinkedList<Block>();   
+        for (Block block : blockSet.getAffectedBlocks(conv)) {
+            if (Randomizer.nextDouble() < pDoubleMove) {
+                // Put ´block´ on both edges with probability ´pDoubleMove´
+                blockSet.addBlockMove(convNew, block);
+                
+                logHGF -= Math.log(pDoubleMove);
+                
+            } else {                
+                // Otherwise decide whether ´block´ is moved to convNew or stays on conv
+                if (Randomizer.nextBoolean()) {
+                    blockSet.addBlockMove(convNew, block);
+                    blocksToRemove.add(block);
+                }
+                
+                logHGF -= Math.log(1 - pDoubleMove);
+                logHGF -= Math.log(0.5);
+            }
+        }
+        
+        for (Block block : blocksToRemove)
+            blockSet.removeBlockMove(conv, block);
+
+        assert acg.getConvCount() == nConv + 1;
+        assert !acg.isInvalid();
+        
+        return logHGF;
+    }
+    
+    protected double mergeProposal() {
+        double logHGF = 0;
+        int nConv = acg.getConvCount();
+
+        // The merge move requires at least two existing conversions        
+        if (nConv < 2)
+            return Double.NEGATIVE_INFINITY;
+
+        // Choose two random conversions
+        Conversion conv1 = acg.getConversions().getRandomConversion();
+        Conversion conv2;
+        do {
+            conv2 = acg.getConversions().getRandomConversion();
+        } while (conv2 == conv1);
+        
+        if (flipInput.get())
+            flipDirection(conv2);
+        
+        // Operator fails if conversions don't share the same nodes
+        if (conv1.getNode1() != conv2.getNode1() || conv1.getNode2() != conv2.getNode2())
+            return Double.NEGATIVE_INFINITY;
+        
+        // Update Hastings ratio
+        //        logHGF -= Math.log(1.0/(nConv*(nConv-1)));
+        //        logHGF += Math.log(1.0/(nConv-1));
+        logHGF += Math.log(nConv);
+
+        
+        
+        // Update Hastings ratio with back-probability for height
+        double hMin = Math.max(conv1.getNode1().getHeight(),
+                               conv1.getNode2().getHeight());
+        double hMax = Math.min(conv1.getNode1().getParent().getHeight(),
+                               conv1.getNode2().getParent().getHeight());
+        logHGF += Math.log(1.0/(hMax - hMin));
+
+        
+        
+        
+        // Update Hastings ratio with back-probability for sampled block moves 
+        double pDoubleMove = pMoveInput.get().getValue();
+        
+        for (Block block : blockSet.getAffectedBlocks(conv1)) {
+            if (block.isAffected(conv2)) {
+                // Blocks affected by conv1 and conv2  ->  reverse move is a duplication move
+                logHGF += Math.log(pDoubleMove);
+            } else {
+                // Blocks affected by conv1 but not conv2  ->  reverse move is no duplication
+                logHGF += Math.log(1 - pDoubleMove);
+                logHGF += Math.log(0.5);
+            }
+        }
+        
+        for (Block block : blockSet.getAffectedBlocks(conv2)) {
+            if (!block.isAffected(conv1)) {
+                // Blocks affected by conv1 but not conv2  ->  reverse move is no duplication
+                logHGF += Math.log(1 - pDoubleMove);
+                logHGF += Math.log(0.5);
+                
+                blockSet.addBlockMove(conv1, block);
+            }
+//            blockSet.removeBlockMove(conv2, block);
+        }
+
+        // Remove conv2
+        blockSet.removeConversion(conv2);   
+        acg.removeConversion(conv2);
+        
+        assert acg.getConvCount() == nConv - 1;
+        assert !acg.isInvalid();
+
+        return logHGF;
+    }
+
+    /**
+     * Flip direction (i.e. swap node1 and node2) of the given conversion
+     * @param conv
+     */
+    private void flipDirection(Conversion conv) {
+        Node node1 = conv.getNode1();
+        Node node2 = conv.getNode2();
+        conv.setNode1(node2);
+        conv.setNode2(node1);
+    }
+    
+}
