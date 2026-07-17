9a476fd8b38da409f0695213caf86b6b4325caa1
Nico Neureiter <nico.neureiter@gmail.com>
2026-07-17 17:47:27 +0200

    Chaged folder structure for  SingleOperatorTest + minor fixes

--- src/test/java/contactrees/test/operators/CFOperatorsSimulationTest.java
+++ src/test/java/contactrees/test/operators/CFOperatorsSimulationTest.java
@@ -2,7 +2,7 @@ package contactrees.test.operators;
 
 import org.junit.Test;
 
-public class CFOperatorsSimulationTest extends TestSingleOperator {
+public class CFOperatorsSimulationTest extends SingleOperatorTest {
 	
     @Test
     public void testWilsonBalding() throws Exception {
